2daad801a02a609ed92aa271da37db52baf396b1
Paulo Vanderley <vanderley.1109@gmail.com>
2023-06-22 15:40:10 -0300

    adicionando conteudo

---
+++ Animal.java
@@ -0,0 +1,31 @@
+package br.com.devsgeeknerd.zoologico.classes;
+
+public class Animal {
+    private String nome;
+    private String especie;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
